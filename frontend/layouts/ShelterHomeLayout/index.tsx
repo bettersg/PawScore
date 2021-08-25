e40f3180ea043070bd6bf00c637088df43b8c23a
shawn <shawn.wee90@hotmail.com>
2021-08-25 23:44:55 +0800

    [FE][Shelter Layout] broke up components for readability

--- frontend/layouts/ShelterHomeLayout/index.tsx
+++ frontend/layouts/ShelterHomeLayout/index.tsx
@@ -12,119 +12,13 @@ const ShelterHomeLayout = ({ children }) => {
 		<>
 			<Layout>
 				<Header className={styles.header}>
-					<div
-						className="logo"
-						style={{ float: "left", width: "100px" }}>
-						<Image src={logo} alt="PawScore Logo" />
-					</div>
-					<Menu
-						style={{ float: "right" }}
-						theme="light"
-						mode="horizontal"
-						defaultSelectedKeys={["shelter"]}>
-						<Menu.Item key="shelter">
-							About PawScore for Shelters
-						</Menu.Item>
-						<Menu.Item key="adopt">I&apos;m looking to adopt pets instead</Menu.Item>
-						<Menu.Item key="login">
-							<Button type="primary">Login</Button>
-						</Menu.Item>
-						<Menu.Item key="register">
-							<Button type="default">Register</Button>
-						</Menu.Item>
-					</Menu>
+					<HeaderContent />
 				</Header>
 				<Content>
 					<div>{children}</div>
 				</Content>
 				<Footer className={styles.footer}>
-					<Row>
-						<Col span={20} offset={2}>
-							<Row>
-								<Col span={24}>
-									<hr className={styles.hr} />
-									<Row>
-										<Col
-											span={6}
-											className={styles.footerCol}>
-											<div className={styles.footerLogo}>
-												<Image
-													src={logo}
-													alt="PawScore Logo"
-												/>
-											</div>
-										</Col>
-										<Col
-											span={6}
-											className={styles.footerCol}>
-											PAWSCORE
-										</Col>
-										<Col
-											span={6}
-											className={styles.footerCol}>
-											FOR ADOPTERS
-										</Col>
-										<Col
-											span={6}
-											className={styles.footerCol}>
-											FOR SHELTERS & PET STORES
-										</Col>
-									</Row>
-									<Row>
-										<Col
-											span={6}
-											className={styles.footerCol}>
-											Social Media Logos
-										</Col>
-										<Col
-											span={6}
-											className={styles.footerCol}>
-											<Link href="#">About Us</Link>
-											<br />
-											<Link href="#">Contact Us</Link>
-											<br />
-											<Link href="#">
-												Terms of service
-											</Link>
-											<br />
-											<Link href="#">Privacy policy</Link>
-											<br />
-										</Col>
-										<Col
-											span={6}
-											className={styles.footerCol}>
-											<Link href="#">Adopt A Pet</Link>
-											<br />
-											<Link href="#">
-												Make A Donation
-											</Link>
-											<br />
-										</Col>
-										<Col
-											span={6}
-											className={styles.footerCol}>
-											<Link href="#">
-												Partner with us
-											</Link>
-											<br />
-											<Link href="#">
-												Terms of service
-											</Link>
-											<br />
-											<Link href="#">Privacy policy</Link>
-											<br />
-										</Col>
-									</Row>
-									<hr className={styles.hr} />
-								</Col>
-							</Row>
-							<Row>
-								<Col span={24} style={{ textAlign: "center" }}>
-									&copy;2021 PawScore. All rights reserved.
-								</Col>
-							</Row>
-						</Col>
-					</Row>
+					<FooterContent />
 				</Footer>
 			</Layout>
 		</>
@@ -132,3 +26,90 @@ const ShelterHomeLayout = ({ children }) => {
 };
 
 export default ShelterHomeLayout;
+
+const HeaderContent = () => (
+	<>
+		<div className="logo" style={{ float: "left", width: "100px" }}>
+			<Image src={logo} alt="PawScore Logo" />
+		</div>
+		<Menu
+			style={{ float: "right" }}
+			theme="light"
+			mode="horizontal"
+			defaultSelectedKeys={["shelter"]}>
+			<Menu.Item key="shelter">About PawScore for Shelters</Menu.Item>
+			<Menu.Item key="adopt">
+				I&apos;m looking to adopt pets instead
+			</Menu.Item>
+			<Menu.Item key="login">
+				<Button type="primary">Login</Button>
+			</Menu.Item>
+			<Menu.Item key="register">
+				<Button type="default">Register</Button>
+			</Menu.Item>
+		</Menu>
+	</>
+);
+
+const FooterContent = () => (
+	<Row>
+		<Col span={20} offset={2}>
+			<Row>
+				<Col span={24}>
+					<hr className={styles.hr} />
+					<Row>
+						<Col span={6} className={styles.footerCol}>
+							<div className={styles.footerLogo}>
+								<Image src={logo} alt="PawScore Logo" />
+							</div>
+						</Col>
+						<Col span={6} className={styles.footerCol}>
+							PAWSCORE
+						</Col>
+						<Col span={6} className={styles.footerCol}>
+							FOR ADOPTERS
+						</Col>
+						<Col span={6} className={styles.footerCol}>
+							FOR SHELTERS & PET STORES
+						</Col>
+					</Row>
+					<Row>
+						<Col span={6} className={styles.footerCol}>
+							Social Media Logos
+						</Col>
+						<Col span={6} className={styles.footerCol}>
+							<Link href="#">About Us</Link>
+							<br />
+							<Link href="#">Contact Us</Link>
+							<br />
+							<Link href="#">Terms of service</Link>
+							<br />
+							<Link href="#">Privacy policy</Link>
+							<br />
+						</Col>
+						<Col span={6} className={styles.footerCol}>
+							<Link href="#">Adopt A Pet</Link>
+							<br />
+							<Link href="#">Make A Donation</Link>
+							<br />
+						</Col>
+						<Col span={6} className={styles.footerCol}>
+							<Link href="#">Partner with us</Link>
+							<br />
+							<Link href="#">Terms of service</Link>
+							<br />
+							<Link href="#">Privacy policy</Link>
+							<br />
+						</Col>
+					</Row>
+					<hr className={styles.hr} />
+				</Col>
+			</Row>
+			<Row>
+				<Col span={24} style={{ textAlign: "center" }}>
+					&copy;2021 PawScore. All rights reserved.
+				</Col>
+			</Row>
+		</Col>
+	</Row>
+);
