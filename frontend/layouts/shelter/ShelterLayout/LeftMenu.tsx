41c6be38747c4a24db5542e80abb3f9138b20272
kyanrong <kekyanrong@gmail.com>
2021-09-07 17:10:19 +0800

    [FE][LeftMenu] Removed styled component

--- frontend/layouts/shelter/ShelterLayout/LeftMenu.tsx
+++ frontend/layouts/shelter/ShelterLayout/LeftMenu.tsx
@@ -1,32 +1,5 @@
 import { Menu } from "antd";
 // import Link from "next/link";
-import styled from "styled-components";
-
-const LeftMenuContainer = styled(Menu)`
-	&& .ant-menu-item-selected,
-	.ant-menu-light .ant-menu-item:hover {
-		color: var(--color-golden-purple) !important;
-		background-color: var(--color-light-purple) !important;
-	}
-
-	&& .ant-menu-item-selected:after {
-		border-right: 3px solid var(--color-golden-purple) !important;
-	}
-
-	&& .ant-menu-item-active,
-	.ant-menu-item:active {
-		color: var(--color-golden-purple) !important;
-	}
-
-	&& .ant-menu-item:active {
-		background: var(--color-light-purple) !important;
-	}
-
-	&& .ant-menu-item a:hover,
-	.ant-menu-item a:active {
-		color: var(--color-golden-purple) !important;
-	}
-`;
 
 type TMenuItem = {
 	key: string;
@@ -60,14 +33,14 @@ const menuItems: TMenuItem[] = [
 
 const LeftMenu = () => {
 	return (
-		<LeftMenuContainer mode="inline">
+		<Menu mode="inline">
 			{menuItems.map((item) => (
 				<Menu.Item key={item.key} onClick={item.onClick}>
 					{item.label}
 					{/* <Link href={`/shelter/home/${item.key}`}>{item.label}</Link> */}
 				</Menu.Item>
 			))}
-		</LeftMenuContainer>
+		</Menu>
 	);
 };
 
