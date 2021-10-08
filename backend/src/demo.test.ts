8cbe0530dee5718cf4e97b192ba467dbbe0bcade
stonefruit <jeromeng888@gmail.com>
2021-10-08 22:14:15 +0800

    add jest testing and github action to test and compile typescript for frontend and backend

---
+++ backend/src/demo.test.ts
@@ -0,0 +1,7 @@
+describe("Demo test file so there will be more than 0 tests. Remove when real tests are available.", () => {
+	test("a test", () => {
+		expect(true).toBe(true);
+	});
+});
+
+export {};
