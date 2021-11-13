d988ccf89bb0a0782f0f9b13853ef57c3955bab6
stonefruit <jeromeng888@gmail.com>
2021-11-13 22:25:51 +0800

    Add tests for abilities (i.e. permissions)

---
+++ backend/src/authorization/tests/abilities.test.ts
@@ -0,0 +1,48 @@
+import abilities from "../abilities";
+import { allActions, allSubjects } from "../types";
+import { User as UserType } from "../../models/user";
+
+const { defineRulesFor } = abilities;
+
+test("Unauthenticated user has some permissions", () => {
+	const allowable: { [key: string]: string[] } = {
+		// Animal: ["read:all"] or something like that
+	};
+	const ability = defineRulesFor();
+	allSubjects.forEach((subject) => {
+		const allowedActionsForSubject = allowable[subject] || [];
+		allActions.forEach((action) => {
+			if (allowedActionsForSubject.includes(action)) {
+				expect(ability.can(action, subject)).toBe(true);
+			} else {
+				expect(ability.can(action, subject)).toBe(false);
+			}
+		});
+	});
+});
+
+test("ADMIN has all permissions", () => {
+	const ability = defineRulesFor({ roles: ["ADMIN"] } as UserType);
+	allSubjects.forEach((subject) => {
+		allActions.forEach((action) => {
+			expect(ability.can(action, subject)).toBe(true);
+		});
+	});
+});
+
+test("Basic authenticated user has some permissions", () => {
+	const allowable: { [key: string]: string[] } = {
+		UserProfile: ["create:self", "read:self", "update:self"]
+	};
+	const ability = defineRulesFor({ roles: ["A TEST ROLE"] } as UserType);
+	allSubjects.forEach((subject) => {
+		const allowedActionsForSubject = allowable[subject] || [];
+		allActions.forEach((action) => {
+			if (allowedActionsForSubject.includes(action)) {
+				expect(ability.can(action, subject)).toBe(true);
+			} else {
+				expect(ability.can(action, subject)).toBe(false);
+			}
+		});
+	});
+});
