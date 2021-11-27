79a371fceddefb084a51d93ec8e6d92c2413c8cb
ryhuz <shawn.wee90@hotmail.com>
2021-11-27 15:15:44 +0800

    [MISC] fixed TS errors with imports

--- backend/src/controllers/healthcheck.ts
+++ backend/src/controllers/healthcheck.ts
@@ -1,11 +1,11 @@
-import { Species } from "../../../contract";
+import { Animal } from "@contract";
 import { Controller, Get } from "routing-controllers";
 
 @Controller("/api/healthcheck")
 export class HealthCheckController {
 	@Get("/")
 	async get(): Promise<string> {
-		return `very healthy, species is ${Species.Dog}`;
+		return `very healthy, species is ${Animal.Species.Dog}`;
 	}
 }
 
