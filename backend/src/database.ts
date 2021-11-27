12ccd6e267477a8e74a7f98b81d948b427b463d6
stonefruit <jeromeng888@gmail.com>
2021-11-27 13:53:36 +0800

    add db connection check on start up

--- backend/src/database.ts
+++ backend/src/database.ts
@@ -1,6 +1,23 @@
 import { Sequelize } from "sequelize";
 import allConfig from "./config/config";
 
-// eslint-disable-next-line 
+// eslint-disable-next-line
 const config: any = allConfig.databaseConfig;
-export const sequelize = new Sequelize(config.database, config.username, config.password, config);
+export const sequelize = new Sequelize(
+	config.database,
+	config.username,
+	config.password,
+	config,
+);
+
+const testConnection = async () => {
+	try {
+		await sequelize.authenticate();
+		console.log("Connection has been established successfully.");
+	} catch (error) {
+		console.error("Unable to connect to the database:", error);
+		throw error;
+	}
+};
+
+testConnection();
