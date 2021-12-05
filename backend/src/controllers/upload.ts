398ea519653d4d1ed16c58bb54cb8271a41a6d46
qroll <quek.ruoling@gmail.com>
2021-12-05 22:26:53 +0800

    [BE][Image upload] Refactor upload endpoint

--- backend/src/controllers/upload.ts
+++ backend/src/controllers/upload.ts
@@ -1,67 +1,75 @@
-import express from "express";
 import { Storage } from "@google-cloud/storage";
-import stream from "stream";
-import { Readable } from "stream";
-import fullConfig from "../config/config";
 import crypto from "crypto";
+import express from "express";
+import { StatusCodes } from "http-status-codes";
+import stream, { Readable } from "stream";
+import { pipeline } from "stream/promises";
+import { z } from "zod";
+import fullConfig from "../config/config";
 import { Upload } from "../models/upload";
+
+const MAX_UPLOAD_MB = 10;
 
 let storage = new Storage();
 
 if (fullConfig.googleSvcAcctKey !== "") {
-  storage = new Storage({keyFilename: fullConfig.googleSvcAcctKey});
+  storage = new Storage({ keyFilename: fullConfig.googleSvcAcctKey });
 }
 
-const storageBucketName = fullConfig.storageBucketName
+const storageBucketName = fullConfig.storageBucketName;
+
+const UploadRequestQuerySchema = z.object({
+  originalFileName: z.string(),
+  base64File: z
+    .string()
+    .max(MAX_UPLOAD_MB * 1024 * 1024, "File size too large"),
+});
 
 class UploadController {
   async upload(req: express.Request, res: express.Response) {
-    // check first before proceeding
-    if (req.body.originalFilename === undefined || req.body.originalFilename === "") {
-      const result = {"status": "failure", "message": "Invalid original filename"};
-      return res.end(JSON.stringify(result));
-    }
+    try {
+      const input = UploadRequestQuerySchema.parse(req.body);
 
-    const MAX_UPLOAD_MB = 10;
-    if (req.body.base64File.length / 4 * 3 > MAX_UPLOAD_MB * 1024 * 1024) {
-      const result = {"status": "failure", "message": "File size too large"};
-      return res.end(JSON.stringify(result));
-    }
+      const bufferStream = new stream.PassThrough();
+      bufferStream.end(Buffer.from(input.base64File, "base64"));
 
-    const bufferStream = new stream.PassThrough();
-    bufferStream.end(Buffer.from(req.body.base64File, 'base64'));
+      //Define bucket.
+      const myBucket = storage.bucket(storageBucketName);
+      //Define file & file name.
+      const newFileName = makeRandomName();
+      const file = myBucket.file(newFileName);
 
-    //Define bucket.
-    const myBucket = storage.bucket(storageBucketName);
-    //Define file & file name.
-    const newFileName = makeRandomName();
-    const file = myBucket.file(newFileName);
-    //Pipe the 'bufferStream' into a 'file.createWriteStream' method.
-    bufferStream.pipe(file.createWriteStream())
-      .on('error', function(err) {
-        console.log("Error during file upload: " + err.toString())
-        console.log(err);
-        const result = {"status": "failure", "message": "Failed to upload file"};
-        return res.end(JSON.stringify(result));
-      })
-      .on('finish', function() {
-        // create object in database
-        const data = {
-          userId: req.user.id,
-          originalFilename: req.body.originalFilename,
-          filename: newFileName,
-        };
-        Upload.create(data).then((newUpload: Upload) => {
-          if (!newUpload) {
-            const result = {"status": "failure", "message": "Failed to create upload reference"};
-            return res.end(JSON.stringify(result));
-          }
-          if (newUpload) {
-            const result = {"status": "success", "message": "You have successfully uploaded the file", "payload": newUpload};
-            return res.end(JSON.stringify(result));
-          }
+      //Pipe the 'bufferStream' into a 'file.createWriteStream' method.
+      try {
+        await pipeline(bufferStream, file.createWriteStream());
+      } catch (err) {
+        console.log("Error during file upload: " + err.message);
+        throw new Error("Failed to upload file");
+      }
+
+      const uploadRecord = {
+        userId: req.user.id,
+        originalFilename: req.body.originalFilename,
+        filename: newFileName,
+      };
+      const newUpload = await Upload.create(uploadRecord);
+
+      return res
+        .status(StatusCodes.OK)
+        .json({
+          message: "You have successfully uploaded the file",
+          payload: newUpload,
         });
-      });
+    } catch (error) {
+      if (error instanceof z.ZodError) {
+        return res
+          .status(StatusCodes.BAD_REQUEST)
+          .json({ message: error.message });
+      }
+      return res
+        .status(StatusCodes.INTERNAL_SERVER_ERROR)
+        .json({ message: error.message });
+    }
   }
 
   async download(req: express.Request, res: express.Response) {
