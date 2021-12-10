9c1018e22baef17ac569a501fe32d958445363b0
qroll <quek.ruoling@gmail.com>
2021-12-11 02:17:46 +0800

    [BE][Image upload] Install file-type, update config for ESM modules

--- backend/webpack.config.js
+++ backend/webpack.config.js
@@ -1,39 +1,47 @@
-const path = require('path');
-const nodeExternals = require('webpack-node-externals');
-const NodemonPlugin = require('nodemon-webpack-plugin');
+import NodemonPlugin from "nodemon-webpack-plugin";
+import path from "path";
+import { fileURLToPath } from "url";
+import nodeExternals from "webpack-node-externals";
 
-module.exports = {
+const dirname = path.dirname(fileURLToPath(import.meta.url));
 
+export default {
     // bundling mode
-    mode: 'production',
+    mode: "production",
 
     devtool: "source-map",
 
     // entry point for app
-    entry: './src/app.ts',
+    entry: "./src/app.ts",
 
     // exclude node_modules
-    externals: [nodeExternals()],
+    externals: [nodeExternals({ importType: "module" })],
     externalsPresets: {
-        node: true
+        node: true,
+    },
+    externalsType: "module",
+
+    experiments: {
+        outputModule: true,
     },
 
     // output bundles (location)
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'app.js'
+        path: path.resolve(dirname, "dist"),
+        filename: "app.js",
+        environment: { module: true },
     },
 
     optimization: {
-        minimize: false
+        minimize: false,
     },
 
     // file resolutions
     resolve: {
-        extensions: ['.ts', '.js'],
+        extensions: [".ts", ".js"],
         alias: {
-            "@contract": path.resolve(__dirname, '../contract')
-        }
+            "@contract": path.resolve(dirname, "../contract"),
+        },
     },
 
     // loaders
@@ -41,15 +49,19 @@ module.exports = {
         rules: [
             {
                 test: /\.ts/,
-                use: 'ts-loader',
+                use: "ts-loader",
                 exclude: /node_modules/,
-            }
-        ]
+            },
+        ],
     },
 
     plugins: [
         new NodemonPlugin({
-            nodeArgs: ["--enable-source-maps", "--inspect"]
-        })
-    ]
+            nodeArgs: [
+                "--enable-source-maps",
+                "--inspect",
+                "--experimental-specifier-resolution=node",
+            ],
+        }),
+    ],
 };
