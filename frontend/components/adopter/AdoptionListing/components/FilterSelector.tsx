046861e6462fd9f1b4e06367cf543cc88768dc3e
qroll <quek.ruoling@gmail.com>
2021-12-20 01:52:54 +0800

    [FE][Pets page] Implement custom selector

--- frontend/components/adopter/AdoptionListing/components/FilterSelector.tsx
+++ frontend/components/adopter/AdoptionListing/components/FilterSelector.tsx
@@ -1,34 +1,92 @@
 import { CaretDownFilled } from "@ant-design/icons";
-import { Button, Dropdown, Menu } from "antd";
+import { Button, Checkbox, Col, Divider, Row, Typography } from "antd";
+import { useState } from "react";
 import styled from "styled-components";
 
-interface FilterSelectorProps {
+interface FilterSelectorProps<T> {
     label: string;
-    options: string[];
-    onChange: (option: string) => void;
+    selections: {
+        title?: string;
+        options: { label: string; value: T }[];
+    }[];
+    onChange: (options: T[]) => void;
 }
 
-export function FilterSelector(props: FilterSelectorProps) {
-    const { label, options, onChange } = props;
+export function FilterSelector<T>(props: FilterSelectorProps<T>) {
+    const { label, selections, onChange } = props;
+    const [isOpen, setIsOpen] = useState(false);
 
     return (
-        <Selector
-            overlay={
-                <Menu>
-                    <Menu.Item>Test</Menu.Item>
-                </Menu>
-            }
-        >
-            <SelectorButton size="large">
+        <div style={{ position: "relative" }}>
+            <SelectorButton size="large" onClick={(e) => setIsOpen(true)}>
                 {label} <CaretDownFilled style={{ color: "#858C94" }} />
             </SelectorButton>
-        </Selector>
+            {isOpen && (
+                <SelectorDropdown>
+                    <CheckboxSection>
+                        <Checkbox.Group onChange={onChange}>
+                            {selections.map((selection) => (
+                                <Row
+                                    key={selection.title}
+                                    gutter={[16, 16]}
+                                    style={{ marginBottom: 18 }}
+                                >
+                                    {selection.title && (
+                                        <Col span={24}>
+                                            <SectionHeading>
+                                                {selection.title}
+                                            </SectionHeading>
+                                        </Col>
+                                    )}
+                                    {selection.options.map((option) => (
+                                        <Col span={24} key={option.value}>
+                                            <Checkbox value={option.value}>
+                                                {option.label}
+                                            </Checkbox>
+                                        </Col>
+                                    ))}
+                                </Row>
+                            ))}
+                        </Checkbox.Group>
+                    </CheckboxSection>
+                    <Divider style={{ margin: 0 }} />
+                    <Button
+                        type="primary"
+                        onClick={(e) => setIsOpen(false)}
+                        style={{ float: "right", margin: "10px 16px" }}
+                    >
+                        Done
+					</Button>
+                </SelectorDropdown>
+            )}
+        </div>
     );
 }
-
-const Selector = styled(Dropdown)``;
 
 const SelectorButton = styled(Button)`
 	border: 2px solid #222261;
 	border-radius: 8px;
+`;
+
+const SectionHeading = styled(Typography.Title)`
+	&& {
+		color: #5e38ba;
+		font-size: 16px;
+		margin: 0;
+	}
+`;
+
+const SelectorDropdown = styled.div`
+	filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+	border: 1px solid #dadee3;
+	border-radius: 4px;
+	position: absolute;
+	background-color: #fff;
+	margin-top: 10px;
+	z-index: 1;
+	width: 253px;
+`;
+
+const CheckboxSection = styled.div`
+	padding: 8px 16px 0 16px;
 `;
--- frontend/components/adopter/AdoptionListing/index.tsx
+++ frontend/components/adopter/AdoptionListing/index.tsx
@@ -65,9 +65,38 @@ const MOCK_ANIMAL_DATA = [
     },
 ] as Animal.Attributes[];
 
+enum SpeciesFilter {
+    Cat,
+    Dog,
+    Others,
+}
+
+enum AgeFilter {
+    Above0Months,
+    Above6Months,
+    Above12Months,
+    Above36Months,
+    Above72Months,
+    Above120Months,
+}
+
+enum GenderFilter {
+    F,
+    M,
+}
+
+enum BreedFilter {
+    AmericanBulldog = "American bulldog",
+}
+
 function AdoptionListingPage() {
     const [animals, setAnimals] =
         useState<Animal.Attributes[]>(MOCK_ANIMAL_DATA);
+
+    const [speciesFilter, setSpeciesFilter] = useState<SpeciesFilter[]>([]);
+    const [ageFilter, setAgeFilter] = useState<AgeFilter[]>([]);
+    const [genderFilter, setGenderFilter] = useState<GenderFilter[]>([]);
+    const [breedFilter, setBreedFilter] = useState<BreedFilter[]>([]);
 
     return (
         <Page>
@@ -82,23 +111,73 @@ function AdoptionListingPage() {
             <Space size="large" wrap style={{ marginTop: "30px" }}>
                 <FilterSelector
                     label="Species"
-                    options={[""]}
-                    onChange={() => { }}
+                    selections={[
+                        {
+                            title: "",
+                            options: [
+                                { label: "Cat", value: SpeciesFilter.Cat },
+                            ],
+                        },
+                    ]}
+                    onChange={(filter) => setSpeciesFilter(filter)}
                 />
                 <FilterSelector
                     label="Age"
-                    options={[""]}
-                    onChange={() => { }}
+                    selections={[
+                        {
+                            title: "Under a year",
+                            options: [
+                                {
+                                    label: "1-6 months",
+                                    value: AgeFilter.Above0Months,
+                                },
+                                {
+                                    label: "7-12 months",
+                                    value: AgeFilter.Above6Months,
+                                },
+                            ],
+                        },
+                        {
+                            title: "Beyond a year",
+                            options: [
+                                {
+                                    label: "1-3 years",
+                                    value: AgeFilter.Above12Months,
+                                },
+                                {
+                                    label: "4-6 years",
+                                    value: AgeFilter.Above36Months,
+                                },
+                            ],
+                        },
+                    ]}
+                    onChange={(filter) => setAgeFilter(filter)}
                 />
                 <FilterSelector
                     label="Gender"
-                    options={[""]}
-                    onChange={() => { }}
+                    selections={[
+                        {
+                            options: [
+                                { label: "Female", value: GenderFilter.F },
+                                { label: "Male", value: GenderFilter.M },
+                            ],
+                        },
+                    ]}
+                    onChange={(filter) => setGenderFilter(filter)}
                 />
                 <FilterSelector
                     label="Breed"
-                    options={[""]}
-                    onChange={() => { }}
+                    selections={[
+                        {
+                            options: [
+                                {
+                                    label: BreedFilter.AmericanBulldog,
+                                    value: BreedFilter.AmericanBulldog,
+                                },
+                            ],
+                        },
+                    ]}
+                    onChange={(filter) => setBreedFilter(filter)}
                 />
                 {/* <div>More filters</div> */}
             </Space>
@@ -143,8 +222,4 @@ const Searchbar = styled(Input)`
 	&:hover {
 		background-color: #f4f6f9;
 	}
-`;
-
-const FilterSelectorGroup = styled.div`
-	margin: 1.5rem 0;
 `;
