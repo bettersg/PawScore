c1e17cfd65630c31c2f2c6ee1580102b2a985873
qroll <quek.ruoling@gmail.com>
2021-12-20 19:58:04 +0800

    [FE][Pets page] Adjust layout for responsiveness
    - Mitigate dropdown rendering outside of viewport
    - Limit columns on larger screens

--- frontend/components/adopter/ViewPets/index.tsx
+++ frontend/components/adopter/ViewPets/index.tsx
@@ -1,11 +1,19 @@
 import { SearchOutlined } from "@ant-design/icons";
 import { Animal } from "@contract";
-import { Input, Space, Typography } from "antd";
+import { Col, Input, Row, Space, Typography } from "antd";
+import AdopterLayout from "layouts/adopter/AdopterLayout";
 import { useState } from "react";
 import styled from "styled-components";
 import { AnimalListing } from "./components/AnimalListing";
 import { FilterSelector } from "./components/FilterSelector";
-import { AgeFilter, AgeFilterOptions, BreedFilterOptions, GenderFilter, GenderFilterOptions, SpeciesFilterOptions } from "./data/filters";
+import {
+    AgeFilter,
+    AgeFilterOptions,
+    BreedFilterOptions,
+    GenderFilter,
+    GenderFilterOptions,
+    SpeciesFilterOptions,
+} from "./data/filters";
 
 const MOCK_ANIMAL_DATA = [
     {
@@ -77,71 +85,81 @@ function AdoptionListingPage() {
     const [breedFilter, setBreedFilter] = useState<number[]>([]);
 
     return (
-        <Page>
-            <PageHeading>Adopt an Animal</PageHeading>
-            <Searchbar
-                placeholder="Search e.g. try 'British Shorthair'"
-                prefix={<SearchOutlined style={{ color: "#FFAC7E" }} />}
-                size="large"
-                bordered={false}
-                style={{ marginTop: "30px" }}
-            />
-            <Space size="large" wrap style={{ marginTop: "30px" }}>
-                <FilterSelector
-                    label="Species"
-                    values={speciesFilter}
-                    selections={SpeciesFilterOptions}
-                    onChange={(filter) => setSpeciesFilter(filter)}
-                />
-                <FilterSelector
-                    label="Age"
-                    values={ageFilter}
-                    selections={AgeFilterOptions}
-                    onChange={(filter) => setAgeFilter(filter)}
-                />
-                <FilterSelector
-                    label="Gender"
-                    selections={GenderFilterOptions}
-                    values={genderFilter}
-                    onChange={(filter) => setGenderFilter(filter)}
-                />
-                <FilterSelector
-                    label="Breed"
-                    selections={BreedFilterOptions}
-                    values={breedFilter}
-                    onChange={(filter) => setBreedFilter(filter)}
-                />
-                {/* <div>More filters</div> */}
-            </Space>
-            <Grid>
-                {animals.map((animal) => (
-                    <AnimalListing key={animal.id} animal={animal} />
-                ))}
-            </Grid>
-        </Page>
+        <AdopterLayout>
+            <Background>
+                <Page>
+                    <PageHeading>Adopt an Animal</PageHeading>
+                    <Searchbar
+                        placeholder="Search e.g. try 'British Shorthair'"
+                        prefix={<SearchOutlined style={{ color: "#FFAC7E" }} />}
+                        size="large"
+                        bordered={false}
+                    />
+                    <Space
+                        size="large"
+                        wrap
+                        style={{
+                            marginBottom: 51,
+                            // leave enough space for dropdown to render within the viewport
+                            maxWidth: "max(1px, calc(100vw - 253px))",
+                        }}
+                    >
+                        <FilterSelector
+                            label="Species"
+                            values={speciesFilter}
+                            selections={SpeciesFilterOptions}
+                            onChange={(filter) => setSpeciesFilter(filter)}
+                        />
+                        <FilterSelector
+                            label="Age"
+                            values={ageFilter}
+                            selections={AgeFilterOptions}
+                            onChange={(filter) => setAgeFilter(filter)}
+                        />
+                        <FilterSelector
+                            label="Gender"
+                            selections={GenderFilterOptions}
+                            values={genderFilter}
+                            onChange={(filter) => setGenderFilter(filter)}
+                        />
+                        <FilterSelector
+                            label="Breed"
+                            selections={BreedFilterOptions}
+                            values={breedFilter}
+                            onChange={(filter) => setBreedFilter(filter)}
+                        />
+                        {/* <div>More filters</div> */}
+                    </Space>
+                    <Row gutter={[26, 26]}>
+                        {animals.map((animal) => (
+                            <Col key={animal.id} xs={24} sm={12} lg={8}>
+                                <AnimalListing animal={animal} />
+                            </Col>
+                        ))}
+                    </Row>
+                </Page>
+            </Background>
+        </AdopterLayout>
     );
 }
 
 export default AdoptionListingPage;
 
-const Page = styled.div`
-	padding: 78px 15%;
-	max-width: 1440px;
+const Background = styled.div`
+	padding: 78px 26px;
 	background-color: #fff;
 `;
 
-const Grid = styled.div`
-	display: grid;
-	grid-template-columns: repeat(3, 1fr);
-	column-gap: 26px;
-	row-gap: 26px;
-	margin-top: 50px;
+const Page = styled.div`
+	max-width: 1150px;
+	margin: auto;
 `;
 
 const PageHeading = styled(Typography.Title)`
 	&& {
 		color: #797777;
 		font-family: Poppins;
+		margin-bottom: 30px;
 	}
 `;
 
@@ -149,6 +167,7 @@ const Searchbar = styled(Input)`
 	background-color: #f4f6f9;
 	border-color: #f4f6f9;
 	border-radius: 0.5rem;
+	margin-bottom: 30px;
 
 	&:focus,
 	&:hover {
