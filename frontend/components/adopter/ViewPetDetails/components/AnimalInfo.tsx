427123e3c8582c3e90a4f8eb69019ac88681444f
qroll <quek.ruoling@gmail.com>
2022-01-21 19:57:58 +0800

    [FE][View pet details] Show animal details

--- frontend/components/adopter/ViewPetDetails/components/AnimalInfo.tsx
+++ frontend/components/adopter/ViewPetDetails/components/AnimalInfo.tsx
@@ -12,6 +12,7 @@ import relativeTime from "dayjs/plugin/relativeTime";
 import Link from "next/link";
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
+import { formatAge, formatValue } from "../data/formatDisplayValues";
 
 dayjs.extend(relativeTime);
 
@@ -249,26 +250,63 @@ export function AboutSection(props: AnimalInfoProps) {
 	);
 }
 
-// TODO: show values
 export function DetailsSection(props: AnimalInfoProps) {
+	const { animal } = props;
+	const size = formatValue(animal.sizeCm, (val) => val + " cm");
+	const age = formatValue(animal.dateOfBirth, formatAge);
+	const breed = formatValue(animal.breed);
+	const gender = formatValue(animal.gender, (val) =>
+		val === "F" ? "Female" : "Male",
+	);
+	const color = formatValue(animal.color);
+	const weight = formatValue(animal.weightKg, (val) => val + " kg");
+	const furLength = formatValue(animal.furLength);
+
 	return (
 		<Section>
 			<Header>More details</Header>
 			<Space wrap size="large">
-				<DetailCard>Species</DetailCard>
-				<DetailCard>Size</DetailCard>
-				<DetailCard>Age</DetailCard>
-				<DetailCard>Breed</DetailCard>
-				<DetailCard>Gender</DetailCard>
-				<DetailCard>Color</DetailCard>
+				<DetailCard>
+					<div>Species</div>
+					<div>{animal.species}</div>
+				</DetailCard>
+				<DetailCard>
+					<div>Size</div>
+					<div>{size}</div>
+				</DetailCard>
+				<DetailCard>
+					<div>Age</div>
+					<div>{age}</div>
+				</DetailCard>
+				<DetailCard>
+					<div>Breed</div>
+					<div>{breed}</div>
+				</DetailCard>
+				<DetailCard>
+					<div>Gender</div>
+					<div>{gender}</div>
+				</DetailCard>
+				<DetailCard>
+					<div>Color</div>
+					<div>{color}</div>
+				</DetailCard>
+				<DetailCard>
+					<div>Weight</div>
+					<div>{weight}</div>
+				</DetailCard>
+				<DetailCard>
+					<div>Fur length</div>
+					<div>{furLength}</div>
+				</DetailCard>
 			</Space>
 		</Section>
 	);
 }
 
-const DetailCard = styled.div`
+const DetailCard = styled.button`
 	background: #ffffff;
 	box-shadow: 0px 0px 6px rgba(48, 27, 150, 0.2);
+	border: none;
 	border-radius: 10px;
 	width: 125px;
 	height: 125px;
@@ -278,7 +316,33 @@ const DetailCard = styled.div`
 	display: flex;
 	align-items: center;
 	justify-content: center;
-	text-transform: uppercase;
+	position: relative;
+
+	& > div {
+		position: absolute;
+		padding: 8px;
+	}
+
+	& > div:first-child {
+		text-transform: uppercase;
+	}
+
+	&:hover > div:first-child,
+	&:active > div:first-child,
+	&:focus > div:first-child {
+		opacity: 0;
+	}
+
+	& > div:last-child {
+		opacity: 0;
+		word-break: break-word;
+	}
+
+	&:hover > div:last-child,
+	&:active > div:last-child,
+	&:focus > div:last-child {
+		opacity: 1;
+	}
 `;
 
 export function ShelterSection(props: AnimalInfoProps) {
